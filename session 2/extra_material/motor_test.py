7493da39d00b4d48436298f3ca6dfedc05e87084
Aksat Shah <as10509@ic.ac.uk>
2018-10-22 13:58:05 +0100

    Updated extra meterial code

--- session 2/extra_material/motor_test.py
+++ session 2/extra_material/motor_test.py
@@ -1,18 +1,20 @@
-from gpiozero import Robot, DistanceSensor, LED
-from time import sleep
-#Import the library with all the functions needed
-led = LED(pin=18)
-#Use the LED object, calling it 'led'
-led.blink(0.5, 0.5,  None, True)
-#on for 0.5 seconds, off for 0.5 seconds, run continuously, run in the background
+import time
+import Robot
 
-leftPins = (12, 16)
-rightPins = (23, 24)
-#Define what BCM pins the motor drivers are connected to
+LEFT_TRIM = 0
+RIGHT_TRIM = 0
 
-robot = Robot(left=leftPins, right=rightPins)
-#Define the robot object
+my_robot = Robot.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)
 
-while True:
-    # Continuously perform the following function
-    robot.forward()
+my_robot.forward(150, 1.0)   # Move forward at speed 150 for 1 second. 
+Speed is a value between 0 and 255
+my_robot.left(200, 0.5) # Spin left at speed 200 for 0.5 seconds.
+
+# Spin in place slowly for a few seconds.
+my_robot.right(100)  # No time is specified so the robot will start 
+spinning forever.
+time.sleep(2.0)   # Pause for a few seconds while the robot spins (you could do other processing here though!).
+
+my_robot.backward(100,2)
+
+my_robot.stop()      # Stop the robot from moving.
